c9a1843a3c5d63fe42d1f535d8ab780d7f32ca71
Sunil Bista <072bex444@ioe.edu.np>
2020-07-29 23:02:37 +0545

    made component folder and added component cardlist and search

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './cardList.css';
+
+export const CardList = (props) => {
+  return (
+    <>
+      <div className='cardList'>{props.children}</div>
+    </>
+  );
+};
