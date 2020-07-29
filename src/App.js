1d3cc354d342c9086b67961ea0b200bee4603a5c
Sunil Bista <072bex444@ioe.edu.np>
2020-07-29 23:03:19 +0545

    adds cardlist component and renders monster data as children

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { CardList } from "./components/CardList/CardList";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       monsters: [],
     };
   }
 
-componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(users => this.setState({
-    monsters: users
-  }))
-}
-
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState({
+          monsters: users,
+        })
+      );
+  }
 
   render() {
     const { monsters } = this.state;
-  
+
     return (
       <div className="App">
-       {
-        monsters.map(monster =>  (<h1 key={monster.id}> {monster.name} </h1>))
-       }
+        <CardList>
+        {monsters.map((monster) => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
+        </CardList>
       </div>
     );
   }
